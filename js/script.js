55a910d8a968217a5165583e6bf275a0c801c060
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-01-23 18:32:49 -0500

    RESPONSIVENESS : Applied media queries for tablet, laptop & large laptop displays.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let closeMenu = document.querySelector('.menu-close');
 let buttonFilters = document.querySelector('.button-filters');
 let filterFields = document.querySelector('.filter-options');
 
+let userIcon = document.querySelector('.user-image');
+
 openMenu.addEventListener('click', () => {
     document.querySelector(".side-bar").style.width = "17em";
 });
@@ -14,4 +16,8 @@ closeMenu.addEventListener('click', () => {
 
 buttonFilters.addEventListener('click', () => {
     filterFields.classList.toggle('filter-options');
+});
+
+userIcon.addEventListener('click', () => {
+    document.querySelector(".side-bar").style.width = "17em";
 });
